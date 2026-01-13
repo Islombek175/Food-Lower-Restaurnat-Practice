90e32a2e4e9761bd24e5693a3da0a6cc702a68cd
Islombek Matkarimov <matkaimovislambek@gmail.com>
2026-01-13 23:10:13 +0500

    №25. Amaliyot. Date va Loader

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
+	// Tabs
+
 	const tabs = document.querySelectorAll('.tabheader__item'),
 		tabsParent = document.querySelector('.tabheader__items'),
 		tabContents = document.querySelectorAll('.tab_content')
@@ -34,4 +36,73 @@ window.addEventListener('DOMContentLoaded', () => {
 	})
 
 	hideTabs()
+
+	//  Loader
+	const loaderWrapper = document.querySelector('.loader-wrapper')
+
+	setTimeout(() => {
+		loaderWrapper.style.display = 'none'
+	}, 2000)
+
+	// Timer
+	const deadline = '2026-02-10'
+
+	function formatNumber(number) {
+		if (number >= 0 && number < 10) {
+			return `0${number}`
+		} else {
+			return number
+		}
+	}
+
+	function getTimeRemaining(endTime) {
+		let days, hours, minutes, seconds
+		const time = Date.parse(endTime) - Date.parse(new Date('2028-01-01'))
+
+		if (time <= 0) {
+			days = 0
+			hours = 0
+			minutes = 0
+			seconds = 0
+		} else {
+			;(days = Math.floor(time / (1000 * 60 * 60 * 24))),
+				(hours = Math.floor((time / (1000 * 60 * 60)) % 24)),
+				(minutes = Math.floor((time / 60000) % 60)),
+				(seconds = Math.floor((time / 1000) % 60))
+		}
+
+		return {
+			totalTime: time,
+			days,
+			hours,
+			minutes,
+			seconds,
+		}
+	}
+
+	function setClock(selector, endTime) {
+		const timer = document.querySelector(selector),
+			days = timer.querySelector('#days'),
+			hours = timer.querySelector('#hours'),
+			minutes = timer.querySelector('#minutes'),
+			seconds = timer.querySelector('#seconds'),
+			timeInterval = setInterval(updateClock, 1000)
+
+		updateClock()
+
+		function updateClock() {
+			const time = getTimeRemaining(endTime)
+
+			days.textContent = formatNumber(time.days)
+			hours.textContent = formatNumber(time.hours)
+			minutes.textContent = formatNumber(time.minutes)
+			seconds.textContent = formatNumber(time.seconds)
+
+			if (time.totalTime <= 0) {
+				clearInterval(timeInterval)
+			}
+		}
+	}
+
+	setClock('.timer', deadline)
 })
