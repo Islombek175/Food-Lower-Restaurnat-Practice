f5c8ad8acfd957260f3dbddf760036b508fbaac4
Islombek <matkaimovislambek@gmail.com>
2026-01-26 11:04:15 -0700

    №35. Amaliyot. Fetch Telegram BOT

--- js/script.js
+++ js/script.js
@@ -109,7 +109,6 @@ window.addEventListener('DOMContentLoaded', () => {
 	// Modal
 	const modalOpenBtns = document.querySelectorAll('[data-modal]'),
 		modal = document.querySelector('.modal'),
-		modalCloseBtn = document.querySelector('[data-modal-close]'),
 		modalContent = document.querySelector('.modal__content')
 
 	function openModal() {
@@ -130,10 +129,11 @@ window.addEventListener('DOMContentLoaded', () => {
 		btn.addEventListener('click', openModal)
 	})
 
-	modalCloseBtn.addEventListener('click', closeModal)
-
 	modal.addEventListener('click', event => {
-		if (event.target === modal) {
+		if (
+			event.target === modal ||
+			event.target.getAttribute('data-modal-close') === ''
+		) {
 			closeModal()
 		}
 	})
@@ -237,4 +237,84 @@ window.addEventListener('DOMContentLoaded', () => {
 			'.offers-items',
 		).render()
 	})
+
+	// FORM
+	const form = document.querySelector('form'),
+		telegramBotToken = '8415822897:AAHoX0oqmXAXdOj6wK0r1abujsgDy8DwI-U',
+		chatId = '6480933576'
+
+	const message = {
+		loading: 'Loading...',
+		success: 'Thanks for contacting us!',
+		failure: 'Something went wrong...',
+	}
+
+	form.addEventListener('submit', event => {
+		event.preventDefault()
+
+		const loader = document.createElement('div')
+		loader.classList.add('loader')
+		loader.style.width = '20px'
+		loader.style.height = '20px'
+		loader.style.marginTop = '20px'
+		form.append(loader)
+
+		const formData = new FormData(form)
+
+		const object = {}
+		formData.forEach((value, key) => {
+			object[key] = value
+		})
+
+		fetch(`https://api.telegram.org/bot${telegramBotToken}/sendMessage`, {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json',
+			},
+			body: JSON.stringify({
+				chat_id: chatId,
+				text: `
+				Name: ${object.name}, Phone: ${object.phone}`,
+			}),
+		})
+			.then(() => {
+				showStatusMessage(message.success)
+				form.reset()
+			})
+			.catch(() => {
+				showStatusMessage(message.failure)
+			})
+			.finally(() => {
+				setTimeout(() => {
+					loader.remove()
+				}, 2000)
+			})
+	})
+
+	function showStatusMessage(message) {
+		const modalDialog = document.querySelector('.modal__dialog')
+
+		modalDialog.classList.add('hide')
+		openModal()
+
+		const statusModal = document.createElement('div')
+		statusModal.classList.add('modal__dialog')
+		statusModal.innerHTML = `
+		<div class="modal__content">
+			<div data-modal-close class="modal__close">&times;</div>
+			<div class="modal__title">
+            ${message}
+      </div>
+		</div>
+		`
+
+		document.querySelector('.modal').append(statusModal)
+
+		setTimeout(() => {
+			statusModal.remove()
+			modalDialog.classList.add('show')
+			modalDialog.classList.remove('hide')
+			closeModal()
+		}, 4000)
+	}
 })
